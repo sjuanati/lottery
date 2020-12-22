ec6bd52d11645c4cea6829c952355dc6ccda512b
Sergi Juanati <wint3rmute@gmail.com>
2020-12-22 09:29:17 +0100

    Added createBet()

---
+++ contracts/Lottery.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.12;
+
+contract Lottery {
+    enum State {IDLE, BETTING}
+    State public currentState = State.IDLE;
+    address payable[] public players;
+    uint256 public betCount; // count # of required participants in lottery round
+    uint256 public betSize; // amount of ether that each participate needs to send to participate in the lottery
+    uint256 public houseFee;
+    address public admin;
+
+    constructor(uint256 fee) public {
+        require((fee > 1 && fee < 100), "fee should be between 1 to 99");
+        houseFee = fee;
+        admin = msg.sender;
+    }
+
+    // count:   # of participants required to distribute the winning
+    // size:    pot size that each participant needs to send
+    // only 1 bet at a time
+    function createBet(uint256 count, uint256 size)
+        external
+        payable
+        inState(State.IDLE)
+        onlyAdmin()
+    {
+        betCount = count;
+        betSize = size;
+        currentState = State.BETTING;
+    }
+
+    modifier inState(State state) {
+        require(currentState == state, "current state does not allow this");
+        _;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "only admin");
+        _;
+    }
+}
